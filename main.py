6cabd3b656b7b6e3e669103f643c6f4cf25b69d1
Alexander de Squilly Nilly Jamestown the Umpteenth Goonlord Foster <7709@balcarras.gloucs.sch.uk>
2024-09-16 14:29:57 +0000

    built basic login and signup prototypes

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 app.config["SECRET_KEY"]="brownstorm"
 
+#routing
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
 
